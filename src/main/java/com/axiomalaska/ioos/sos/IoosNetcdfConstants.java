a56860714cd4aee35392b31427a662d2cca0c3a6
Shane StClair <shane@axiomalaska.com>
2013-11-25 23:09:17 -0800

    Added IoosNetcdfConstants.SHORT_NAME

--- src/main/java/com/axiomalaska/ioos/sos/IoosNetcdfConstants.java
+++ src/main/java/com/axiomalaska/ioos/sos/IoosNetcdfConstants.java
@@ -5,4 +5,9 @@ public class IoosNetcdfConstants {
      * For platform_variable, holds the stationID
      */
     public static final String IOOS_CODE = "ioos_code";
+
+    /**
+     * For platform_variable, holds the shortName
+     */
+    public static final String SHORT_NAME = "short_name";
 }
