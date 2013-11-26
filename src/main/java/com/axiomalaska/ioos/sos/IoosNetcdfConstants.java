0962d13fa85c4ee4d7e4af21ccfadd7fcd23be32
Shane StClair <shane@axiomalaska.com>
2013-11-25 23:00:32 -0800

    Added IoosNetcdfConstants

---
+++ src/main/java/com/axiomalaska/ioos/sos/IoosNetcdfConstants.java
@@ -0,0 +1,8 @@
+package com.axiomalaska.ioos.sos;
+
+public class IoosNetcdfConstants {
+    /**
+     * For platform_variable, holds the stationID
+     */
+    public static final String IOOS_CODE = "ioos_code";
+}
